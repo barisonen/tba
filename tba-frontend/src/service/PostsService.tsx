92f81f891734d056be5a77e6975c15df06a05c8a
barisonen <barisonen.1996@gmail.com>
2023-02-22 21:42:19 +0300

    c

--- tba-frontend/src/service/PostsService.tsx
+++ tba-frontend/src/service/PostsService.tsx
@@ -1,6 +1,11 @@
 import axios from "axios";
 import AxiosConfigModel from "../interface/AxiosConfigModel";
 import CreatePostModel from "../interface/CreatePostModel";
+import {storage} from "../config/firebase";
+import {ref, listAll, getDownloadURL} from "firebase/storage";
+
+// todo: return ederken liste bos gidiyor pushlarken sorun yok. ya bunu duzelt yada diger sinifta post uzerinden
+// todo: cekmeye don
 
 const config: AxiosConfigModel = {
   baseUrl: "http://localhost:8080/posts",
@@ -22,6 +27,23 @@ export const getPosts = async (): Promise<any> => {
       data: error.response
     }
   })
+}
+
+export const getImages = async (): Promise<any> => {
+  const imageListRef = ref(storage, "images/");
+  let images: any[] = [];
+  listAll(imageListRef).then((response) => {
+    response.items.forEach((item) => {
+      getDownloadURL(item).then((url) => {
+        images.push(url);
+        console.log(images)
+      });
+
+      console.log(images);
+      return images;
+  })}).catch((error) => {
+    console.log(error);
+  });
 }
 
 export const createPost = async (post: CreatePostModel): Promise<any> => {
